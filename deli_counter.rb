b5fd247516b5531246aa0a4fccaf41ef80e8d29d
Tony Ho <ho.tonym@gmail.com>
2018-05-03 17:40:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,24 +12,23 @@ counter = 1
         string += " #{counter}. #{person}"
           counter +=1
       end
-              puts "The line is currently:#{string}"
+      puts "The line is currently:#{string}"
   end
-
 end
 
 
 def take_a_number (array,last_person)
 
-  array<<(last_person)
-  index = (array.index(last_person)) + 1
-  puts "Welcome, #{last_person}. You are number #{index} in line."
+    array << (last_person)
+    array.length
+    index = array[-1] + 1
+    puts "Welcome, #{last_person}. You are number #{index} in line."
 
 end
 
 def now_serving(array)
 
-
-if array.length > 0
+  if array.length > 0
       puts "Currently serving #{array[0]}."
         array.shift
   else
